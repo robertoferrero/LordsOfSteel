b7310f3ba8f5a50a34ccd5dc3879a9648f9e5e4c
Roberto Ferrero <roberto.ferrero.pintor@gmail.com>
2023-05-04 10:33:01 +0200

    Creats 4 personatges inicials i ArrayList

--- src/lordsofsteel/LordsOfSteel.java
+++ src/lordsofsteel/LordsOfSteel.java
@@ -4,6 +4,8 @@
  */
 package lordsofsteel;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author rferrero
@@ -15,14 +17,27 @@ public class LordsOfSteel {
      */
     public static void main(String[] args) {
         
-        //Arma arma = new Arma("Daga");
-        Nan n1 = new Nan(8,6, 13,16,16,new Arma("Daga"));
-        
-        // Personatge normal: (8+5)/4 = 3
-        // Nan: (8+6+5)/4 = 4
+        // Personatge normal: pd = (8+5)/4 = 3
+        // Nan: pd = (8+6+5)/4 = 4
         // Nan: ps = (8+6) = 14
-        System.out.println("Punts de dany de n1: " + n1.getPd());
-        System.out.println("Punts de salut de n1: " + n1.getPs());
+        //System.out.println("Punts de dany de n1: " + n1.getPd());
+        //System.out.println("Punts de salut de n1: " + n1.getPs());
+        
+        //Arma arma = new Arma("Daga");
+        Nan n1 = new Nan(8,6, 13,16,16,new Arma("Daga"));        
+        Huma h1 = new Huma(13,16,6,10,6,new Arma("Espasa"));
+        Mitja mi1 = new Mitja(9,7,7,15,16,new Arma("Martell"));
+        Maia ma1 = new Maia(7,9, 12,15,17,new Arma("Daga"));
+        
+        /*Personatge[] personatges = new Personatge[4];
+        personatges[0] = n1;
+        personatges[1] = h1;*/
+        ArrayList<Personatge> personatges = new ArrayList<Personatge>();
+        personatges.add(n1);
+        personatges.add(h1);
+        personatges.add(mi1);
+        personatges.add(ma1);
+        
     }
     
 }
